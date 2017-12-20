958b003b4f128f700eb879a9d68b799d7248b713
Kevin Yokley <kyokley2@gmail.com>
2017-12-19 20:58:48 -0600

    Adding setup files

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name='encrypt_file',
+    description="Simple script to encrypt a given file",
+    author="Kevin Yokley",
+    author_email='kyokley2@gmail.com',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(exclude=['tests']),
+    package_data={},
+    include_package_data=True,
+    entry_points={},
+    scripts=['scripts/encrypt_file'],
+    test_suite='nose.collector',
+    install_requires=[],
+    tests_require=[
+    ],
+    zip_safe=False,
+    keywords='crypto',
+    classifiers=[
+        'Development Status :: 5 - Released',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        # An invalid classifier disables uploads to PyPI; DevPi doesn't care.
+        'Private :: Do Not Upload',
+    ],
+)
