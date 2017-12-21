0ad632ad9cbc0ae8c2021c0cfdd8c00c1c111c9a
Kevin Yokley <kyokley2@gmail.com>
2017-12-20 18:51:00 -0600

    Adding decrypt method

--- src/encrypt_file/main.py
+++ src/encrypt_file/main.py
@@ -6,6 +6,10 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+SALT_LENGTH = 16
+KEY_LENGTH = 32
+HASH_ITERATIONS = 100000
+
 def encrypt(password, input_data):
     if not isinstance(password, bytes):
         raise ValueError('password must be of type bytes')
@@ -13,12 +17,11 @@ def encrypt(password, input_data):
     if not isinstance(input_data, bytes):
         raise ValueError('input_data must be of type bytes')
 
-    import pdb; pdb.set_trace()
-    salt = os.urandom(16)
+    salt = os.urandom(SALT_LENGTH)
     kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),
-                     length=32,
+                     length=KEY_LENGTH,
                      salt=salt,
-                     iterations=100000,
+                     iterations=HASH_ITERATIONS,
                      backend=default_backend())
 
     key = base64.urlsafe_b64encode(kdf.derive(password))
@@ -29,3 +32,26 @@ def encrypt(password, input_data):
     encoded_salt = base64.urlsafe_b64encode(salt)
 
     return encoded_salt + b'$' + output_data
+
+def decrypt(password, ciphertext):
+    if not isinstance(password, bytes):
+        raise ValueError('password must be of type bytes')
+
+    if not isinstance(ciphertext, bytes):
+        raise ValueError('ciphertext must be of type bytes')
+
+    encoded_salt, data = ciphertext.split(b'$')
+
+    salt = base64.urlsafe_b64decode(encoded_salt)
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),
+                     length=KEY_LENGTH,
+                     salt=salt,
+                     iterations=HASH_ITERATIONS,
+                     backend=default_backend())
+
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+
+    f = Fernet(key)
+    plaintext = f.decrypt(data)
+
+    return plaintext
