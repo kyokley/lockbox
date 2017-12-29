01b2239c63f3c4a3a85e1dd0ca64bad0b3e75d17
Kevin Yokley <kyokley2@gmail.com>
2017-12-28 23:44:19 -0600

    Adding functional test

--- tests/functional/test_functional.py
+++ tests/functional/test_functional.py
@@ -1,4 +1,7 @@
-from lockbox.main import _get_fernet
+from lockbox.main import (_get_fernet,
+                          encrypt,
+                          decrypt,
+                          )
 
 from cryptography.fernet import Fernet
 
@@ -6,3 +9,16 @@ class TestGetFernet(object):
     def test_returns_fernet(self):
         fernet_obj = _get_fernet(b'password', b'0' * 16)
         assert isinstance(fernet_obj, Fernet)
+
+class TestEncryptDecryptRoundTrip(object):
+    def test_run(self):
+        password = b'super secret passphrase'
+        plaintext = b'this is a sample plaintext to be encrypted'
+
+        ciphertext = encrypt(password, plaintext)
+
+        assert plaintext != ciphertext
+
+        returned_plaintext = decrypt(password, ciphertext)
+
+        assert plaintext == returned_plaintext
