449644a5d657f87659dfb16ee703ad7b67d47345
Kevin Yokley <kyokley2@gmail.com>
2017-12-29 08:31:31 -0600

    Adding tests for file file encrypt/decrypt

--- tests/functional/test_functional.py
+++ tests/functional/test_functional.py
@@ -1,9 +1,28 @@
+import os
+import tempfile
+import hashlib
 from lockbox.main import (_get_fernet,
                           encrypt,
                           decrypt,
+                          encrypt_file,
+                          decrypt_file,
                           )
 
 from cryptography.fernet import Fernet
+
+ONE_MB = 1024 * 1024 # 1MB
+
+def _get_hash(filename):
+    sha256 = hashlib.sha256()
+
+    with open(filename, 'rb') as f:
+        chunk = f.read(1024)
+        while chunk:
+            sha256.update(chunk)
+            chunk = f.read(1024)
+
+    return sha256.hexdigest()
+
 
 class TestGetFernet(object):
     def test_returns_fernet(self):
@@ -22,3 +41,39 @@ class TestEncryptDecryptRoundTrip(object):
         returned_plaintext = decrypt(password, ciphertext)
 
         assert plaintext == returned_plaintext
+
+class TestEncryptFileDecryptFileRoundTrip(object):
+    def setup_method(self):
+        self.password = b'super secret passphrase'
+        self.plaintext = b'this is a sample plaintext to be encrypted'
+
+    def test_small_file(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            plaintext_filename = os.path.join(tmp_dir, 'plaintext_filename.txt')
+            encrypted_filename = os.path.join(tmp_dir, 'encrypted_filename.txt')
+
+            with open(plaintext_filename, 'wb') as f:
+                f.write(self.plaintext)
+
+            encrypt_file(self.password, plaintext_filename, output_file=encrypted_filename)
+
+            test_filename = os.path.join(tmp_dir, 'test_filename.txt')
+            decrypt_file(self.password, encrypted_filename, output_file=test_filename)
+
+            assert _get_hash(plaintext_filename) == _get_hash(test_filename)
+
+    def test_large_file(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            plaintext_filename = os.path.join(tmp_dir, 'plaintext_filename')
+            encrypted_filename = os.path.join(tmp_dir, 'encrypted_filename')
+
+            with open(plaintext_filename, 'wb') as f:
+                for i in range(50):
+                    f.write(os.urandom(ONE_MB))
+
+            encrypt_file(self.password, plaintext_filename, output_file=encrypted_filename)
+
+            test_filename = os.path.join(tmp_dir, 'test_filename')
+            decrypt_file(self.password, encrypted_filename, output_file=test_filename)
+
+            assert _get_hash(plaintext_filename) == _get_hash(test_filename)
