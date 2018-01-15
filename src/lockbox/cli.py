b05eaee8dfe51cbe3e6eb75e9d607b0642fbfdd7
Kevin Yokley <kyokley2@gmail.com>
2018-01-14 20:39:15 -0600

    Adding recursive to CLI

--- src/lockbox/cli.py
+++ src/lockbox/cli.py
@@ -1,12 +1,20 @@
+import os
 import getpass
 
 from lockbox import (encrypt,
                      decrypt,
                      encrypt_file,
                      decrypt_file,
+                     encrypt_directory,
+                     decrypt_directory,
+                     LockBoxException,
                      )
 
-def cli_encrypt(passphrase, infile, outfile, data=None):
+def cli_encrypt(passphrase,
+                infile,
+                outfile,
+                data=None,
+                recursive=False):
     confirm_passphrase = getpass.getpass('Confirm passphrase: ').encode('utf-8')
 
     if passphrase != confirm_passphrase:
@@ -18,12 +26,28 @@ def cli_encrypt(passphrase, infile, outfile, data=None):
         if stdout_data:
             print(stdout_data.decode('utf-8'))
     else:
-        encrypt_file(passphrase, infile, output_file=outfile)
+        if os.path.isfile(infile):
+            encrypt_file(passphrase, infile, output_file=outfile)
+        elif os.path.isdir(infile):
+            if not recursive:
+                raise LockBoxException('{} is a directory but --recursive has not been specified'.format(infile))
+            else:
+                encrypt_directory(passphrase, infile)
 
-def cli_decrypt(passphrase, infile, outfile, data=None):
+def cli_decrypt(passphrase,
+                infile,
+                outfile,
+                data=None,
+                recursive=False):
     if data:
         stdout_data = decrypt(passphrase, data, outfile=outfile)
         if stdout_data:
             print(stdout_data.decode('utf-8'))
     else:
-        decrypt_file(passphrase, infile, output_file=outfile)
+        if os.path.isfile(infile):
+            decrypt_file(passphrase, infile, output_file=outfile)
+        elif os.path.isdir(infile):
+            if not recursive:
+                raise LockBoxException('{} is a directory but --recursive has not been specified'.format(infile))
+            else:
+                decrypt_directory(passphrase, infile)
