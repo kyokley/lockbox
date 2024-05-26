730a7b985be3d425e24016105de7196201da8af8
Kevin Yokley <kyokley2@gmail.com>
2024-05-26 07:10:32 -0500

    Add nix files

---
+++ flake.nix
@@ -0,0 +1,40 @@
+# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
+#
+# SPDX-License-Identifier: CC0-1.0
+
+{
+  description = "Easy cryptographic functions";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        customOverrides = self: super: {
+          # Overrides go here
+        };
+
+        app = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+          overrides =
+            [ pkgs.poetry2nix.defaultPoetryOverrides customOverrides ];
+        };
+
+        # DON'T FORGET TO PUT YOUR PACKAGE NAME HERE, REMOVING `throw`
+        packageName = "lockbox";
+      in {
+        packages.${packageName} = app;
+
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ poetry ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
+}
