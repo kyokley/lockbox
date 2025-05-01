f69cb86114c76d39fea795ac7a8a047150edf37e
Kevin Yokley <kyokley2@gmail.com>
2025-05-01 07:21:23 -0500

    Add devenv

---
+++ devenv.nix
@@ -0,0 +1,48 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "lockbox";
+
+  # https://devenv.sh/packages/
+  packages = [];
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    version = "3.12";
+    venv.enable = true;
+    uv.enable = true;
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo Welcome to $GREET
+  '';
+
+  enterShell = ''
+    hello
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
